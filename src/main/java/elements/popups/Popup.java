afa40d07abc977de204afebb2b824f8bc1d7817c
konrad <kkol@users.noreply.github.com>
2020-01-19 19:04:36 +0100

    init

---
+++ src/main/java/elements/popups/Popup.java
@@ -0,0 +1,18 @@
+package elements.popups;
+
+
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+
+public class Popup {
+/*    @Getter
+    private WebDriver driver;
+    private FactoryCommons factory;
+
+    public Popup(WebDriver driver){
+        this.driver = driver;
+        factory = new FactoryCommons(driver);
+    }*/
+
+
+}
